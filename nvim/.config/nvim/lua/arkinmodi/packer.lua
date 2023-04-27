e160b2165cef57b3656cec1a4d6dc5c9433b1d5d
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2023-04-27 15:39:38 -0400

    update lsp-zero

--- nvim/.config/nvim/lua/arkinmodi/packer.lua
+++ nvim/.config/nvim/lua/arkinmodi/packer.lua
@@ -32,28 +32,28 @@ return require("packer").startup(function(use)
 	use("navarasu/onedark.nvim")
 	use("rose-pine/neovim")
 	use("folke/tokyonight.nvim")
+	use({ "catppuccin/nvim", as = "catppuccin" })
 
 	-- LSP
 	use({
 		"VonHeikemen/lsp-zero.nvim",
-		branch = "v1.x",
+		branch = "v2.x",
 		requires = {
 			-- LSP Support
 			{ "neovim/nvim-lspconfig" }, -- Required
-			{ "williamboman/mason.nvim" }, -- Optional
+			{
+				-- Optional
+				"williamboman/mason.nvim",
+				run = function()
+					pcall(vim.cmd, "MasonUpdate")
+				end,
+			},
 			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
 			-- Autocompletion
 			{ "hrsh7th/nvim-cmp" }, -- Required
 			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
-			{ "hrsh7th/cmp-buffer" }, -- Optional
-			{ "hrsh7th/cmp-path" }, -- Optional
-			{ "saadparwaiz1/cmp_luasnip" }, -- Optional
-			-- { "rsh7th/cmp-nvim-lua" }, -- Optional
-
-			-- Snippets
 			{ "L3MON4D3/LuaSnip" }, -- Required
-			{ "rafamadriz/friendly-snippets" }, -- Optional
 		},
 	})
 	use("jose-elias-alvarez/null-ls.nvim")
