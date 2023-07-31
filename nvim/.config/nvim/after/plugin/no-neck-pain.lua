49a81d1aba3bfb93a2f114e46a646ab8c0cd51ef
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2023-07-31 12:37:07 -0400

    add check if in headless

--- nvim/.config/nvim/after/plugin/no-neck-pain.lua
+++ nvim/.config/nvim/after/plugin/no-neck-pain.lua
@@ -1,5 +1,7 @@
-require("no-neck-pain").setup({
-	autocmds = {
-		enableOnVimEnter = true,
-	},
-})
+if next(vim.api.nvim_list_uis()) ~= nil then
+	require("no-neck-pain").setup({
+		autocmds = {
+			enableOnVimEnter = true,
+		},
+	})
+end
