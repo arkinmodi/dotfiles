115256d132b15aa8893b17115620a1e60b0520cb
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2023-09-10 09:21:28 -0400

    words

--- nvim/.config/nvim/lua/arkinmodi/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/arkinmodi/plugins/nvim-cmp.lua
@@ -2,6 +2,7 @@ return {
 	"hrsh7th/nvim-cmp",
 	dependencies = {
 		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-nvim-lua",
 		"hrsh7th/cmp-path",
 		{ "L3MON4D3/LuaSnip", version = "2.*" },
