5f7e580348bfdacfbae9eace8e9a43653711f944
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2023-09-23 19:17:18 -0400

    center more commands

--- nvim/.config/nvim/lua/arkinmodi/core/remap.lua
+++ nvim/.config/nvim/lua/arkinmodi/core/remap.lua
@@ -18,6 +18,33 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "move screen down 1/2 page and 
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "move screen up 1/2 page and center cursor" })
 vim.keymap.set("n", "n", "nzzzv", { desc = "repeat search in same direction and center cursor" })
 vim.keymap.set("n", "N", "Nzzzv", { desc = "repeat search in reverse direction and center cursor" })
+vim.keymap.set("n", "%", "%zz", { desc = "move cursor to matching character and center cursor" })
+vim.keymap.set(
+	"n",
+	"*",
+	"*zz",
+	{ desc = "move cursor to next occurrence of hovered word and center cursor" }
+)
+vim.keymap.set(
+	"n",
+	"#",
+	"#zz",
+	{ desc = "move cursor to previous occurrence of hovered word and center cursor" }
+)
+vim.keymap.set("n", "{", "{zz", { desc = "jump to next paragraph and center cursor" })
+vim.keymap.set("n", "}", "}zz", { desc = "jump to previous paragraph and center cursor" })
+vim.keymap.set(
+	"n",
+	"<C-i>",
+	"<C-i>zz",
+	{ desc = "go to newer position in jump list and center cursor" }
+)
+vim.keymap.set(
+	"n",
+	"<C-o>",
+	"<C-o>zz",
+	{ desc = "go to older position in jump list and center cursor" }
+)
 
 vim.keymap.set("n", "Q", "<nop>", { desc = "never use ex mode" })
 vim.keymap.set("n", "x", '"_x', { desc = "delete character without yanking" })
