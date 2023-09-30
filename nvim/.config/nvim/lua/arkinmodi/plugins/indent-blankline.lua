2823aabb3bb10aa1b785ae444ef9967230342596
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2023-09-30 18:59:01 -0400

    fix indent-blankline

--- nvim/.config/nvim/lua/arkinmodi/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/arkinmodi/plugins/indent-blankline.lua
@@ -1,6 +1,7 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
 	event = { "BufReadPost", "BufNewFile" },
+	main = "ibl",
 	opts = {
 		space_char_blankline = " ",
 		show_current_context = true,
