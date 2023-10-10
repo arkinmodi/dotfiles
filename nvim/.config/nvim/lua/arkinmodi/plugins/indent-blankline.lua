2c6fff6760abe91d7df3484a88746940b26d27ec
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2023-10-10 10:42:30 -0400

    fix nvim 0.9.4 errors

--- nvim/.config/nvim/lua/arkinmodi/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/arkinmodi/plugins/indent-blankline.lua
@@ -2,9 +2,5 @@ return {
 	"lukas-reineke/indent-blankline.nvim",
 	event = { "BufReadPost", "BufNewFile" },
 	main = "ibl",
-	opts = {
-		space_char_blankline = " ",
-		show_current_context = true,
-		show_current_context_start = true,
-	},
+	config = true,
 }
