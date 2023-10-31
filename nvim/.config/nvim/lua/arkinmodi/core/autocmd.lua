5f311a05ed9849d86d1b31597968f25172314f07
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2023-10-30 20:40:38 -0400

    update whitespace trimmer to match neovim's

--- nvim/.config/nvim/lua/arkinmodi/core/autocmd.lua
+++ nvim/.config/nvim/lua/arkinmodi/core/autocmd.lua
@@ -1,9 +1,6 @@
-local augroup = vim.api.nvim_create_augroup
-local autocmd = vim.api.nvim_create_autocmd
-
 -- Highlight Yanks
-autocmd("TextYankPost", {
-	group = augroup("HighlightYank", { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("HighlightYank", { clear = true }),
 	pattern = "*",
 	callback = function()
 		vim.highlight.on_yank({
@@ -14,15 +11,20 @@ autocmd("TextYankPost", {
 })
 
 -- Auto-Remove Whitespace
-autocmd("BufWritePre", {
-	group = augroup("Whitespace", { clear = true }),
-	pattern = "*",
-	command = "%s/\\s\\+$//e",
+-- https://github.com/neovim/neovim/blob/6d1a2f2c3c51560555ea6f7867273635d07eb287/runtime/lua/editorconfig.lua#L93-L102
+vim.api.nvim_create_autocmd("BufWritePre", {
+	group = vim.api.nvim_create_augroup("TrimTrailingWhitespace", { clear = true }),
+	callback = function()
+		local view = vim.fn.winsaveview()
+		vim.api.nvim_command("silent! undojoin")
+		vim.api.nvim_command("silent keepjumps keeppatterns %s/\\s\\+$//e")
+		vim.fn.winrestview(view)
+	end,
 })
 
 -- LSP Keymaps
-autocmd("LspAttach", {
-	group = augroup("UserLspConfig", { clear = true }),
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
 	callback = function(ev)
 		-- Enable completion triggered by <c-x><c-o>
 		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
