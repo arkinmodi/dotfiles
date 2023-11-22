301733e378327c8ca99680712e70b6e758459160
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2023-11-21 19:20:19 -0500

    actually use treesitter config and remove terraform lsp
    only reason terraform lsp was added b/c treesitter syntax highlighting was not working, which this fixes

--- nvim/.config/nvim/lua/arkinmodi/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/arkinmodi/plugins/treesitter.lua
@@ -1,22 +1,26 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+	cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+	event = { "BufReadPost", "BufNewFile" },
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter-context",
+		"windwp/nvim-ts-autotag",
 	},
-	cmd = { "TSUpdateSync" },
-	event = { "BufReadPost", "BufNewFile" },
-	opts = {
-		ensure_installed = {
-			"lua",
-			"vim",
-			"vimdoc",
-		},
-		sync_install = false,
-		auto_install = true,
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = { "org" },
-		},
-	},
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"lua",
+				"vim",
+				"vimdoc",
+			},
+			sync_install = false,
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = { "org" },
+			},
+			autotag = { enable = true },
+		})
+	end,
 }
