ea2755a76d5224be58ee5f7b2d3d9052d53f5dd9
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2023-12-20 22:28:17 -0500

    dynamically include linters

--- nvim/.config/nvim/lua/arkinmodi/plugins/lsp/nvim-lint.lua
+++ nvim/.config/nvim/lua/arkinmodi/plugins/lsp/nvim-lint.lua
@@ -2,10 +2,23 @@ return {
 	"mfussenegger/nvim-lint",
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
+		local python_linters = {}
+		if vim.fn.executable("flake8") == 1 then
+			table.insert(python_linters, "flake8")
+		end
+		if vim.fn.executable("mypy") == 1 then
+			table.insert(python_linters, "mypy")
+		end
+
+		local yaml_linters = {}
+		if vim.fn.executable("yamllint") == 1 then
+			table.insert(yaml_linters, "yamllint")
+		end
+
 		local nvim_lint = require("lint")
 		nvim_lint.linters_by_ft = {
-			python = { "flake8" },
-			yaml = { "yamllint" },
+			python = python_linters,
+			yaml = yaml_linters,
 		}
 
 		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
