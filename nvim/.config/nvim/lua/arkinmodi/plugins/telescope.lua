db80f64f85a73ee25a64daf199232414cce595a4
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2024-01-05 20:07:50 -0500

    add search for word under cursor keymap

--- nvim/.config/nvim/lua/arkinmodi/plugins/telescope.lua
+++ nvim/.config/nvim/lua/arkinmodi/plugins/telescope.lua
@@ -3,21 +3,32 @@ return {
 	branch = "0.1.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		"nvim-tree/nvim-web-devicons",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	},
 	keys = {
-		"<leader>pf",
 		"<C-p>",
+		"<leader>pWs",
+		"<leader>pf",
 		"<leader>ps",
+		"<leader>pws",
 	},
 	config = function()
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Telescope find_files" })
 		vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Telescope git_files" })
+
 		vim.keymap.set("n", "<leader>ps", function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
-		end, { desc = "Telescope grep_string" })
+		end, { desc = "Telescope grep string" })
+
+		vim.keymap.set("n", "<leader>pws", function()
+			builtin.grep_string({ search = vim.fn.expand("<cword>") })
+		end, { desc = "Telescope grep word under cusor" })
+
+		vim.keymap.set("n", "<leader>pWs", function()
+			builtin.grep_string({ search = vim.fn.expand("<cWORD>") })
+		end, { desc = "Telescope grep WORD under cusor" })
 
 		require("telescope").load_extension("fzf")
 	end,
