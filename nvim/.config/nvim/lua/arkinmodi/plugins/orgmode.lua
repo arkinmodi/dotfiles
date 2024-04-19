599df016488533237073bb080169bb758508cd17
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2024-04-19 07:13:44 -0400

    remove setup_ts_grammar in orgmode plugin

--- nvim/.config/nvim/lua/arkinmodi/plugins/orgmode.lua
+++ nvim/.config/nvim/lua/arkinmodi/plugins/orgmode.lua
@@ -5,10 +5,7 @@ return {
 		{ "nvim-treesitter/nvim-treesitter", lazy = true },
 	},
 	ft = "org",
-	config = function()
-		require("orgmode").setup_ts_grammar()
-		require("orgmode").setup({
-			org_startup_folded = "showeverything",
-		})
-	end,
+	opts = {
+		org_startup_folded = "showeverything",
+	},
 }
