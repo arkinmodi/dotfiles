9697bfe1f376710975c83c508c96efe618a42c17
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2025-03-30 11:01:32 -0400

    update nvim deps

--- nvim/.config/nvim/lua/arkinmodi/plugins/colours.lua
+++ nvim/.config/nvim/lua/arkinmodi/plugins/colours.lua
@@ -1,31 +1,29 @@
 return {
-	{
-		"Shatur/neovim-ayu",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			local colours = require("ayu.colors")
-			colours.generate()
-			require("ayu").setup({
-				mirage = false,
-				overrides = {
-					GitSignsCurrentLineBlame = { fg = colours.guide_active },
-					IncSearch = { bg = colours.lsp_parameter, fg = colours.selection_inactive },
-					StatusLine = { bg = colours.panel_bg, fg = colours.keyword },
-					Visual = { bg = colours.selection_bg },
+	"Shatur/neovim-ayu",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		local colours = require("ayu.colors")
+		colours.generate()
+		require("ayu").setup({
+			mirage = false,
+			overrides = {
+				GitSignsCurrentLineBlame = { fg = colours.guide_active },
+				IncSearch = { bg = colours.lsp_parameter, fg = colours.selection_inactive },
+				StatusLine = { bg = colours.panel_bg, fg = colours.keyword },
+				Visual = { bg = colours.selection_bg },
 
-					-- transparency
-					FoldColumn = { bg = "None" },
-					Folded = { bg = "None" },
-					Normal = { bg = "None" },
-					NormalFloat = { bg = "None" },
-					SignColumn = { bg = "None" },
-					VertSplit = { bg = "None" },
-					WhichKeyFloat = { bg = "None" },
-					WinSeparator = { bg = "None" },
-				},
-			})
-			vim.cmd.colorscheme("ayu")
-		end,
-	},
+				-- transparency
+				FoldColumn = { bg = "None" },
+				Folded = { bg = "None" },
+				Normal = { bg = "None" },
+				NormalFloat = { bg = "None" },
+				SignColumn = { bg = "None" },
+				VertSplit = { bg = "None" },
+				WhichKeyFloat = { bg = "None" },
+				WinSeparator = { bg = "None" },
+			},
+		})
+		vim.cmd.colorscheme("ayu")
+	end,
 }
